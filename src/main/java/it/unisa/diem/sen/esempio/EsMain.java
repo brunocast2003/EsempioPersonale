9f844e3fdc990b664060a50e7551bca28358f608
ggrie <ggrie@Giovannipc>
2024-11-20 16:57:49 +0100

    push1

--- src/main/java/it/unisa/diem/sen/esempio/EsMain.java
+++ src/main/java/it/unisa/diem/sen/esempio/EsMain.java
@@ -16,7 +16,7 @@ public class EsMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("suca");
+
         System.out.println("Nell'istruzione add $t0, $t1, $t2...");
     }
     
